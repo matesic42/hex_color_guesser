6abd254918540f50c79b068d17860014378e4fc9
matesic42 <markomtsc@gmail.com>
2023-04-13 17:33:07 +0200

    last fixes

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -6,7 +6,7 @@ const Buttons = ({ answers, check }: any) => (
         onClick={() => {
           check(answer);
         }}
-        className="rounded bg-[#31748f] p-3 text-sm font-medium text-[#e0def4] shadow-md shadow-black lg:transition lg:delay-150 lg:duration-300 lg:ease-in-out lg:hover:-translate-y-1 lg:hover:scale-110 hover:cursor-pointer hover:bg-[#f6c177] hover:text-[#26233a] hover:shadow-lg hover:shadow-black"
+        className="rounded bg-[#31748f] p-3 text-sm font-medium text-[#e0def4] shadow-md shadow-black lg:transition lg:delay-150 lg:duration-300 lg:ease-in-out lg:hover:-translate-y-1 lg:hover:scale-110 lg:hover:cursor-pointer lg:hover:bg-[#f6c177] lg:hover:text-[#26233a] lg:hover:shadow-lg lg:hover:shadow-black"
       >
         {answer}
       </div>
